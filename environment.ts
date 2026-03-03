0d538d6c5d10daecc4fe3d5b0a7865ee73758694
Emoji0805 <mamihery.rabiazamaholy@gmail.com>
2026-03-03 07:07:46 +0300

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://m1p13mean-mamihery-mialy-backend.onrender.com'  // URL pour le développement
+  apiUrl: 'https://m1p13mean-mamihery-mialy-backend-prod.onrender.com'  // URL pour le développement
 };
